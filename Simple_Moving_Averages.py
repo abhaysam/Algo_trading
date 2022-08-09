b8962ee673c9e147399afbd197631754ba1d097f
abhay_samm <31345761+abhaysam@users.noreply.github.com>
2022-08-09 03:38:15 +0200

    [ADD] Strategies

---
+++ Simple_Moving_Averages.py
@@ -0,0 +1,26 @@
+# This is the first and the most basic strategy based on Price/Volume Data
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use("seaborn")
+
+eurusd = pd.read_csv("eurusd.csv", parse_dates=["Date"], index_col="Date")
+
+# Visualizing the data
+eurusd.plot(figsize = (12, 8), title = "EUR/USD", fontsize = 12)
+plt.show()
+
+# Adding log returns
+eurusd["returns"] = np.log(eurusd.div(eurusd.shift(1)))
+
+# Dropping NaN
+eurusd.dropna(inplace = True)
+
+# Cumulative returns
+eurusd["cumulative_returns"] = eurusd.returns.cumsum()
+eurusd["cumulative_max"] = eurusd.cumulative_returns.cummax()
+
+# Performance Matrix
+annualized_returns = eurusd.returns.mean()*252
+volatility = eurusd.returns.std() * np.sqrt(252)
+drawdown = (eurusd["cumulative_max"] - eurusd["cumulative_returns"]).max()
