4c226a1824631c38444f221dd8bce2fb9d64e104
abhay_samm <abhaysam@outlook.com>
2022-09-14 22:22:50 +0200

    Update data_downloader.py

--- data_downloader.py
+++ data_downloader.py
@@ -14,13 +14,22 @@ Help for Oanda:
 """
 
 def data_downloader(symbol, start, end, interval = "D", price = "M", frequency = None):
-    # Several pulls needs to be made if the granularith is not daily
-    raw = api.get_history(instrument = symbol,start = start, end = end, granularity = interval, price = price)
+    granularity_available = ['S5','S10','S15','S30','M1','M2','M4','M5','M10','M15','M30','H1','H2','H3','H4','H6','H8','H12','D','W']
+    price_availabe = ['A','B','M']
+    if interval not in granularity_available:
+        raise Exception("This granularity is not availabe")
+    if price not in price_availabe:
+        raise Exception("This price is not availabe")
     print("We are pulling the data from OANDA")
+    if interval == "D" or interval == "W" or interval == "M":
+        raw = api.get_history(instrument = symbol,start = start, end = end, granularity = interval, price = price)
+    else:        
+        # Several pulls needs to be made if the granularith is not daily
+        raw = api.get_history(instrument = symbol,start = start, end = end, granularity = interval, price = price)
     return raw
     
-start = "2004-01-01"
+# start = "2004-01-01"
+# # end = "2020-06-30"
 # end = "2020-06-30"
-end = "2004-06-30"
-symbol = "EUR_USD"
-data = data_downloader(symbol, start, end, "H1", 'B')
+# symbol = "EUR_USD"
+# data = data_downloader(symbol, start, end, "W", 'B')
