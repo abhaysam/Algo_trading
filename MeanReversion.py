88161a4bf2bed88b43fceb78ddfd47c70a89363c
abhay_samm <abhaysam@outlook.com>
2022-10-10 11:56:18 +0200

    Update MeanReversion.py

--- MeanReversion.py
+++ MeanReversion.py
@@ -38,3 +38,23 @@ plt.show()
 #Zooming in on 08/2019
 data.loc["2019-08",["price", "SMA","Lower","Upper"]].plot(figsize = (12,8))
 plt.show()
+
+data.dropna(inplace = True)
+
+#Defining the strategy (Mean - Reversion with Bollinger Bands)
+data["distance"] = data.price - data.SMA
+data["position"] = np.where(data.price < data.Lower, 1,np.nan) #1. Oversold -> go long
+data["position"] = np.where(data.price > data.Upper,-1,data["position"]) #2 Overbought -> go short
+data["position"] = np.where(data.distance * data.distance.shift(1) < 0, 0,data["position"]) #3 The point where price crosses SMA -> stay neutral (no action needed)
+data.position = data.position.ffill().fillna(0)
+
+#Getting a summary of all our positions
+data.position.value_counts()
+
+#Plotting the positions with the SMA
+data.drop(columns=["returns", "distance"]).plot(figsize = (12,8), secondary_y = "position")
+plt.show()
+
+#Zooming in on 08/2019
+data.drop(columns=["returns", "distance"]).loc["2019-08"].plot(figsize = (12,8), secondary_y = "position")
+plt.show()
